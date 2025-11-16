1542463d5f05e63a17abe878ee302eb6d60e4c52
Anbu <anbumca05@gmail.com>
2025-11-16 16:14:54 +0530

    v1

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
-1 sudo yum install java -y
-2 sudo yum install git -y
-3 sudo yum install maven -y
-4
-5 if [ -d "simplejava" ]
-6 then
-7 echo "repo is cloned and exists"
-8 cd /home/ec2-user/simplejava
-9 git pull origin demo1
-10 else
-11 git clone https://github.com/anbalaganramu/simplejava.git
-12 fi
-13
-14 cd /home/ec2-user/simplejava
-15 mvn compile
+sudo yum install java-17-amazon-corretto-devel -y
+sudo yum install git -y
+sudo yum install maven -y
+
+if [ -d "simplejava" ]
+then
+  echo "repo is cloned and exists"
+  cd /home/ec2-user/simplejava
+  git pull origin demo1
+else
+  git clone https://github.com/anbalaganramu/simplejava.git
+fi
+
+cd /home/ec2-user/simplejava
+mvn compile
